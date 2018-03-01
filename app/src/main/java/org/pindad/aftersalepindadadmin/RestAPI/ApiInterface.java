963568083164f526a17544e4ed2d9f67779ab2ab
WIDHIYANTO NUGROHO <widhiyantonugroho10@gmail.com>
2018-03-01 10:45:35 +0700

    List Email v1.0

--- app/src/main/java/org/pindad/aftersalepindadadmin/RestAPI/ApiInterface.java
+++ app/src/main/java/org/pindad/aftersalepindadadmin/RestAPI/ApiInterface.java
@@ -1,5 +1,6 @@
 package org.pindad.aftersalepindadadmin.RestAPI;
 
+import org.pindad.aftersalepindadadmin.Model.Message;
 import org.pindad.aftersalepindadadmin.Model.Users;
 
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.PUT;
 
 /**
@@ -19,4 +21,7 @@ public interface ApiInterface {
     @PUT("login")
     Call<List<Users>> putLogin(@Field("username") String username,
                                @Field("password") String password);
+
+    @GET("inbox.json")
+    Call<List<Message>> getInbox();
 }
